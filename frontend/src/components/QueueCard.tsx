1a6fbce0f69c247951c57bc406f410d1c2b3c0d8
maheswarreddykanala <maheswarreddykanala@gmail.com>
2026-07-15 15:20:47 +0530

    fix: import useRef in QueueCard to resolve build error

--- frontend/src/components/QueueCard.tsx
+++ frontend/src/components/QueueCard.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useRef } from 'react';
 import { cn, safeLocalStorage } from '../utils/index.js';
 
 interface QueueCardProps {
